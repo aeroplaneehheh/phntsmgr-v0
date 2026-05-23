5df6682aebc33eba6bba7e36afa1844079e3df99
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-05-24 01:15:17 +0800

    modified music player to be draggable (finallygit add .! after 100000 years)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import $ from 'jquery';
-import 'jquery-ui/ui/interactions/draggable';
 $(".window-button").click(function() {
     $("#music_player").hide();
 })
 $(".show-button").click(function() {
     $("#music_player").toggle();
+})
+if (typeof jQuery.ui !== 'undefined') {
+    console.log("jQuery UI is loaded");
+} else {
+    console.log("jQuery UI is NOT loaded");
+}
+$(function() {
+    $("#music_player").draggable();
 })
