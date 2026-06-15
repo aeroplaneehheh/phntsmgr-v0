24197488923535675f03865991c24aab40de30b2
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-06-15 21:49:40 +0800

    javascript for the music player

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let trackName = $(".song_name");
+let trackName = $(".track_name");
 let artist = $(".artist");
 let playPauseBtn = $(".play_pause_btn");
-let currentTime = $(".current_time");
+let currTime = $(".current_time");
 let endTime = $(".end_time");
 
 let trackIndex = 0;
@@ -67,4 +67,112 @@ $(".show-button").click(function() {
 })
 $(function() {
     $("#music_player").draggable();
+    $("#music_player").resizable(
+        {
+            alsoResize: ".slider-container"
+        },
+        {
+            minHeight: 360
+        },
+        {
+            maxWidth: 450
+        },
+        {
+            minWidth: 200
+        }
+    );
 })
+
+function loadTrack(trackIndex) {
+    clearInterval(updateTimer);
+    resetValues();
+
+    currentTrack.src = trackList[trackIndex].path;
+    currentTrack.load();
+    $(trackName).text(trackList[trackIndex].name);
+    $(artist).text(trackList[trackIndex].artist);
+
+    updateTimer = setInterval(seekUpdate, 1000);
+    $(currentTrack).on("ended", function() {
+        nextTrack();
+    });
+}
+
+function resetValues() {
+    $(currTime).text("00:00");
+    $(endTime).text("00:00");
+}
+
+function playPauseTrack() {
+    if (! isPlaying) playTrack();
+    else playPauseTrack();
+}
+
+function playTrack() {
+    currentTrack.play();
+    isPlaying = true;
+    $(".play_pause_btn").attr("src", "assets/play.png");
+}
+
+function pauseTrack() {
+    currentTrack.pause();
+    isPlaying = false;
+    $(".play_pause_btn").attr("src", 'assets/stop.png');
+}
+
+function nextTrack() {
+    if (trackIndex < track_list.length - 1) {
+        trackIndex += 1;
+    }
+    else trackIndex = 0;
+
+    loadTrack(trackIndex);
+    playTrack();
+}
+
+function previousTrack() {
+    if (trackIndex > 0) {
+        trackIndex -= 1;
+    }
+    else trackIndex = trackList.length
+
+    loadTrack(trackIndex);
+    playTrack();
+}
+
+
+// current time-- change the current time into a percentage and change
+// the linear gradient with that, subtract and add continuously???
+
+function seekTo() {
+    seekto = currentTrack.duration * (currentTrack.currentTime / 100);
+}
+
+function seekUpdate() {
+    if (!isNaN(currentTrack.duration)) {
+        seekPosition = currentTrack.currentTime * (100 / currentTrack.duration);
+        $(".slider-color").attr("background", "linear-gradient(to right, rgb(179, 171, 138)" + seekPosition + "% , transparent" (100 - seekPosition) + "%");
+    }
+
+    let currentMinutes = Math.floor(currentTrack.currentTime / 60);
+    let currentSeconds = Math.floor(currentTrack.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(currentTrack.duration / 60);
+    let durationSeconds = Math.floor(currentTrack.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) {
+        currentSeconds = "0" + currentSeconds;
+    }
+    if (currentMinutes < 10) {
+        currentMinutes = "0" + currentMinutes;
+    }
+    if (durationSeconds < 10) {
+        durationSeconds = "0" + durationSeconds;
+    }
+    if (durationMinutes < 10) {
+        durationMinutes = "0" + durationMinutes;
+    }
+    $(currTime).text(currentMinutes + ":" + currentSeconds);
+    $(endTime).text(durationMinutes + ":" + durationSeconds);
+    // i guess this is all. i'll check if it works tmr :))) im so sleepy but i still need to work on 
+    // my other project which is a macropad. sigh.
+}
