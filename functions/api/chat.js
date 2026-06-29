794563ace8b4fd5d0aa938b629cd4903c8d752a0
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-06-29 21:34:11 +0800

    j

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,18 +1,9 @@
 export default {
     async fetch(request, env, ctx) {
-        const upgradeHeader = context.request.headers.get("Upgrade");
-    
-    if (!upgradeHeader || upgradeHeader !== "websocket") {
-        const { results } = await context.env.DB.prepare(
-            "SELECT text FROM messages ORDER BY id ASC" 
-        ).all();
-        return new Response(JSON.stringify(results), {
-                headers: { "Content-Type": "application/json" }
-        });
-    }
-    const id = env.CHAT_ROOM.idFromName("global_room");
-    const roomStub = env.CHAT_ROOM.get(id);
-    return roomStub.fetch(request, env);
+        const upgradeHeader = request.headers.get("Upgrade");
+        const id = env.CHAT_ROOM.idFromName("global_room");
+        const roomStub = env.CHAT_ROOM.get(id);
+        return roomStub.fetch(request, env);
     }
 };
 
@@ -20,25 +11,37 @@ export class chatRoom {
     constructor(state, env) {
         this.state = state;
         this.env = env
+
+        this.sql = state.storage.sql;
+        this.sql.exec(`
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                text TEXT NOT NULL
+            );
+        `)
     }
 
     async fetch(request) {
+        const upgradeHeader = request.headers.get("Upgrade");
+        if (upgradeHeader !== "websocket") {
+            const cursor = this.sql.exec("SELECT text FROM messages ORDER BY id ASC");
+            const results = Array.from(cursor);
+            return new Response(JSON.stringify(results), {
+                headers: { "Content-Type": "application/json" }
+        });
+    }
         const [client, server] = new WebSocketPair();
         this.state.acceptWebSocket(server);
-        const { results } = await this.env.DB.prepare(
-            "SELECT text FROM messages ORDER BY id ASC"
-        ).all();
-        for (const row of results) {
+        
+        const cursor = this.sql.exac("SELECT text FROM messages ORDER BY id ASC");
+        for (const row of cursor) {
             server.send(row.text);
         }
-
         return new Response(null, { status: 101, webSocket: client });
     };
 
     async webSocketMessage(ws, message) {
-        await this.env.DB.prepare("INSERT INTO messages (text) VALUES (?)")
-        .bind(message)
-        .run();
+        this.sql.exac("INSERT INTO messages (text) VALUES (?)", message);
 
         const allSockets = this.state.getWebSockets();
         for (const socket of allSockets) {
